aa015d297e51ed3d7b3f088952d82e63f875e3e0
Ben <bmtimbrell@gmail.com>
2023-12-15 21:35:14 +0000

    added dark theme toggle

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,30 @@
 const navigationHeight = document.querySelector('.header').offsetHeight;
-
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 'px');
+
+const themeToggle = document.querySelector('.theme-toggle');
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (JSON.parse(window.localStorage.getItem('theme'))?.dark) {
+    themeToggle.src = './resources/images/light-bulb-off.png';
+    document.body.classList.toggle('dark', true);
+} else if (prefersDark) {
+    themeToggle.src = './resources/images/light-bulb-off.png';
+    document.body.classList.toggle('dark', true);
+} else {
+    themeToggle.src = './resources/images/light-bulb.png';
+    document.body.classList.toggle('dark', false);
+}
+
+themeToggle.addEventListener('click', e => {
+    const imgSrc = e.target.src;
+
+    if (imgSrc.substring(imgSrc.length - 14) === 'light-bulb.png') {
+        e.target.src='./resources/images/light-bulb-off.png';
+        window.localStorage.setItem('theme', JSON.stringify({dark: true}));
+    } else {
+        e.target.src = './resources/images/light-bulb.png';
+        window.localStorage.setItem('theme', JSON.stringify({dark: false}));
+    }
+    
+    document.body.classList.toggle('dark');
+});
